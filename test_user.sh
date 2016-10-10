9f4133e66457bb86e2386be15be7cb87bda2437d
Mikhail Sotnikov <mialsot@gmail.com>
2016-10-10 23:27:24 +0300

    user: POST tests implemented

--- test_user.sh
+++ test_user.sh
@@ -21,7 +21,7 @@ echo "user servlet test: [START]"
 
 echo "GET test suite:"
 
-case="case 1: no user name provided 1"
+case="case 1.1: no user name provided 1"
 
 res=$(curl --silent http://${host_addr}/?${servlet_name}/name=/key=a | head -c 1)
 [ ! $? -eq 0 ] && echo "curl error"
@@ -32,7 +32,7 @@ else
 	echo "[FAIL]: ${case}"
 fi
 
-case="case 2: no user name provided 2"
+case="case 1.2: no user name provided 2"
 res=$(curl --silent http://${host_addr}/?${servlet_name}/key=a | head -c 1)
 [ ! $? -eq 0 ] && echo "curl error"
 if [ $res -eq 1 ];then
@@ -42,7 +42,7 @@ else
 	echo "[FAIL]: ${case}"
 fi
 
-case="case 3: user name is too long"
+case="case 2: user name is too long"
 res=$(curl --silent http://${host_addr}/?${servlet_name}/name=${long_name}/key=a | head -c 1)
 [ ! $? -eq 0 ] && echo "curl error"
 if [ $res -eq 2 ];then
@@ -52,7 +52,7 @@ else
 	echo "[FAIL]: ${case}"
 fi
 
-case="case 4: no key provided 1"
+case="case 3.1: no key provided 1"
 res=$(curl --silent http://${host_addr}/?${servlet_name}/name=${valid_name}/key= | head -c 1)
 [ ! $? -eq 0 ] && echo "curl error"
 if [ $res -eq 3 ];then
@@ -62,7 +62,7 @@ else
 	echo "[FAIL]: ${case}"
 fi
 
-case="case 5: no key provided 2"
+case="case 3.2: no key provided 2"
 res=$(curl --silent http://${host_addr}/?${servlet_name}/name=${valid_name} | head -c 1)
 [ ! $? -eq 0 ] && echo "curl error"
 if [ $res -eq 3 ];then
@@ -72,7 +72,7 @@ else
 	echo "[FAIL]: ${case}"
 fi
 
-case="case 6: key is too long"
+case="case 4: key is too long"
 res=$(curl --silent http://${host_addr}/?${servlet_name}/name=${valid_name}/key=${long_key} | head -c 1)
 [ ! $? -eq 0 ] && echo "curl error"
 if [ $res -eq 4 ];then
@@ -82,7 +82,7 @@ else
 	echo "[FAIL]: ${case}"
 fi
 
-case="case 7: wrong user name provided"
+case="case 5: wrong user name provided"
 res=$(curl --silent http://${host_addr}/?${servlet_name}/name=${wrong_name}/key=${valid_key} | head -c 1)
 [ ! $? -eq 0 ] && echo "curl error"
 if [ $res -eq 5 ];then
@@ -92,7 +92,7 @@ else
 	echo "[FAIL]: ${case}"
 fi
 
-case="case 8: wrong key provided"
+case="case 6: wrong key provided"
 res=$(curl --silent http://${host_addr}/?${servlet_name}/name=${right_name}/key=${valid_key} | head -c 1)
 [ ! $? -eq 0 ] && echo "curl error"
 if [ $res -eq 6 ];then
@@ -101,9 +101,62 @@ else
 	test_fail=$((test_fail+1))
 	echo "[FAIL]: ${case}"
 fi
+#TODO
+#case="case 7: get default admin user"
 echo "tests success: $test_success; tests fail: $test_fail"
 
 echo "POST test suite:"
+test_success=0
+test_fail=0
+
+case="case 1.1:no data provided"
+res=$(curl --silent -X POST -d 1 http://${host_addr}/?${servlet_name} | head -c 1)
+[ ! $? -eq 0 ] && echo "curl error"
+if [ $res -eq 7 ]; then
+	test_success=$((test_success+1))
+else
+	test_fail=$((test_fail+1))
+	echo "[FAIL]: ${case}"
+fi
+case="case 1.2: too long data provided"
+long_data="sssssssssssssssssssooooooooooooooooooommmmmmmmmmmmmmmmmmeeeeeeeeeeeeeeee long DATAAAAAAAAAAA"
+res=$(curl --silent -X POST -d ${long_data} http://${host_addr}/?${servlet_name} | head -c 1)
+[ ! $? -eq 0 ] && echo "curl error"
+if [ $res -eq 7 ]; then
+	test_success=$((test_success+1))
+else
+	test_fail=$((test_fail+1))
+	echo "[FAIL]: ${case}"
+fi
+
+case="case 2: invalid user data provided"
+invalid_user=",some_key,1"
+res=$(curl --silent -X POST -d ${invalid_user} http://${host_addr}/?${servlet_name} | head -c 1)
+[ ! $? -eq 0 ] && echo "curl error"
+if [ "$res" = "8" ]; then
+	test_success=$((test_success+1))
+else
+	test_fail=$((test_fail+1))
+	echo "[FAIL]: ${case}"
+fi
+case="case 3: not new(existent) user data provided"
+existent_user="test_user,some_key,1"
+res=$(curl --silent -X POST -d ${existent_user} http://${host_addr}/?${servlet_name} | head -c 1)
+[ ! $? -eq 0 ] && echo "curl error"
+if [ "$res" = "a" ]; then
+	test_success=$((test_success+1))
+else
+	test_fail=$((test_fail+1))
+	echo "[FAIL]: ${case}"
+fi
+echo "tests success: $test_success; tests fail: $test_fail"
+
+echo "PUT test suite:"
+test_success=0
+test_fail=0
+echo "tests success: $test_success; tests fail: $test_fail"
+
+echo "DELETE test suite:"
 test_success=0
 test_fail=0
 echo "tests success: $test_success; tests fail: $test_fail"
--- users.c
+++ users.c
@@ -29,156 +29,183 @@ kv_t users;
 static int add_admin(kv_t *kv_store, struct F_store *fs_store);
 static int add_fsitem_to_kv(kv_t *kv_store, struct F_item *fs_item);
 static int init_kv_store(struct F_store *fs_store, kv_t *kv_store);
-static int print_each(const kv_item *item, const void *string);
 static void user_to_buf(xbuf_t *buf, struct F_item *user);
 static int get_user_from_req(char *data, u64 data_len, struct F_item *user);
+static void write_all_users(struct F_store *store, xbuf_t *buffer);
 
 int main(int argc, char *argv[])
 {
-		char str[MAX_REPLY] = {0};
-		char *tmp_str = NULL;
-		char store_path[1024] = {0};
-		xbuf_t *reply = get_reply(argv);
+	char str[MAX_REPLY] = {0};
+	char *tmp_str = NULL;
+	char store_path[1024] = {0};
+	xbuf_t *reply = get_reply(argv);
 
-		u64 method = (u64) get_env(argv, REQUEST_METHOD);
+	u64 method = (u64) get_env(argv, REQUEST_METHOD);
 
-		struct F_item req_user = {0};
-		struct F_item *cur_user = NULL;
+	struct F_item req_user = {0};
+	struct F_item *cur_user = NULL;
 
-		if ('\0' == users.name[0]) {
-				// init f_storee
-				printf("store initialized\n");
-				s_snprintf(store_path, sizeof(store_path)-1, "%s/%s",
-					   	(char *)get_env(argv, VHOST_ROOT),
-						STORE_PATH
-				);
-				s_snprintf(fs_store.name, 7, "user");
-				if (init_store(store_path, &fs_store)) {
-					s_snprintf(str, sizeof(str)-1, "[ERROR]: users f_store initialization failed, path =%s", store_path);
-					log_err(argv, str);
-					return HTTP_500_INTERNAL_SERVER_ERROR;
-				}
-				s_snprintf(str, sizeof(str)-1, "[OK]: user f_store of <%d> size initialized", fs_store.size);
+	if ('\0' == users.name[0]) {
+			// init f_storee
+			printf("store initialized\n");
+			s_snprintf(store_path, sizeof(store_path)-1, "%s/%s",
+				   	(char *)get_env(argv, VHOST_ROOT),
+					STORE_PATH
+			);
+			s_snprintf(fs_store.name, 7, "user");
+			if (init_store(store_path, &fs_store)) {
+				s_snprintf(str, sizeof(str)-1, "[ERROR]: users f_store initialization failed, path =%s", store_path);
 				log_err(argv, str);
-				// init kv store
-				kv_init(&users, "users", fs_store.size, 0, 0, 0);
-				if(init_kv_store(&fs_store, &users)) {
-					log_err(argv, "[ERROR]: init_kv_store(): unable to init");
+				return HTTP_500_INTERNAL_SERVER_ERROR;
+			}
+			s_snprintf(str, sizeof(str)-1, "[OK]: user f_store of <%d> size initialized", fs_store.size);
+			log_err(argv, str);
+			// init kv store
+			kv_init(&users, "users", fs_store.size, 0, 0, 0);
+			if(init_kv_store(&fs_store, &users)) {
+				log_err(argv, "[ERROR]: init_kv_store(): unable to init");
+				return HTTP_500_INTERNAL_SERVER_ERROR;
+			}
+			log_err(argv, "[OK]: {users} kv store initialized");
+
+			if (NULL == (struct F_item *) kv_get(&users, ADM, sizeof(ADM)-1)) {
+				if (add_admin(&users, &fs_store)) {
 					return HTTP_500_INTERNAL_SERVER_ERROR;
 				}
-				log_err(argv, "[OK]: {users} kv store initialized");
+			}
+	}
 
-				if (NULL == (struct F_item *) kv_get(&users, ADM, sizeof(ADM)-1)) {
-					if (add_admin(&users, &fs_store)) {
-						return HTTP_500_INTERNAL_SERVER_ERROR;
-					}
-				}
-		}
-
-		// TODO refactor to switch
-		if (1 == method) 
-		{
-			// serve GET request
-			get_arg("name=", &tmp_str, argc, argv);
-			if (NULL == tmp_str || ('\0' == *(tmp_str))) {
-				// no user name provided
-				xbuf_cat(reply, "1:[ERROR]: no user name provided");
-				return HTTP_200_OK;
-			} else if (31 < strlen(tmp_str)) {
-				// user name is tooooo long
-				xbuf_cat(reply, "2:[ERROR]: user name is too long");
-				return HTTP_200_OK;
-			}
-			if (!strcmp(tmp_str, "all")) {
-				// reply all users
-				printf("D0\n");
-				kv_do(&users, NULL, 0, print_each, (void*)reply);
-				return HTTP_200_OK;
-			}
-			strncpy(req_user.name, tmp_str, NAME_SIZE);
-			tmp_str = NULL;
-			get_arg("key=", &tmp_str, argc, argv);
-			if (NULL == tmp_str || ('\0' == *(tmp_str))) {
-				xbuf_cat(reply, "3:[ERROR]: no key provided");
-				return HTTP_200_OK;
-			} else if (31 < strlen(tmp_str)) {
-				xbuf_cat(reply, "4:[ERROR]: key is too long");
-				return HTTP_200_OK;
-			}
-			cur_user = (struct F_item *) kv_get(&users, req_user.name, strlen(req_user.name));
-			if (!cur_user) {
-				xbuf_cat(reply, "5:[ERROR]: no such user in store");
-				return HTTP_200_OK;
-			}
-			if (strcmp(cur_user->key, tmp_str)) {
-				xbuf_cat(reply, "6:[ERROR]: user key is not equal to stored one");
-				return HTTP_200_OK;
-			}
-			user_to_buf(reply, cur_user);
+	if (1 == method) 
+	{
+		// serve GET request
+		get_arg("name=", &tmp_str, argc, argv);
+		if (NULL == tmp_str || ('\0' == *(tmp_str))) {
+			// no user name provided
+			xbuf_cat(reply, "1:[ERROR]: no user name provided");
 			return HTTP_200_OK;
-
+		} else if (31 < strlen(tmp_str)) {
+			// user name is tooooo long
+			xbuf_cat(reply, "2:[ERROR]: user name is too long");
+			return HTTP_200_OK;
 		}
-	   	else if (4 == method || 3 == method)
-	   	{
-			// server POST & PUT request
-			char *req_data = (char *) get_env(argv, REQ_ENTITY);
-			u64 content_len = (u64) get_env(argv, CONTENT_LENGTH);
-			u64 res = 0;
-			if (MAX_REQ_DATA < content_len) {
-				xbuf_cat(reply, "7:[ERROR]: too long data, max len is 63");
-				return HTTP_200_OK;
-			}
-			if ((res = get_user_from_req(req_data, content_len, &req_user))) {
-				xbuf_xcat(reply, "8:[ERROR]: user data is no valid: [%d]", res);
-				return HTTP_200_OK;
-			}
-			cur_user = (struct F_item *) kv_get(&users, req_user.name, strlen(req_user.name));
-			if (cur_user) {
-				// TODO implement update user
-				// update user
+		if (!strcmp(tmp_str, "all")) {
+			// reply all users
+			write_all_users(&fs_store, reply);
+			return HTTP_200_OK;
+		}
+		strncpy(req_user.name, tmp_str, NAME_SIZE);
+		tmp_str = NULL;
+		get_arg("key=", &tmp_str, argc, argv);
+		if (NULL == tmp_str || ('\0' == *(tmp_str))) {
+			xbuf_cat(reply, "3:[ERROR]: no key provided");
+			return HTTP_200_OK;
+		} else if (31 < strlen(tmp_str)) {
+			xbuf_cat(reply, "4:[ERROR]: key is too long");
+			return HTTP_200_OK;
+		}
+		cur_user = (struct F_item *) kv_get(&users, req_user.name, strlen(req_user.name));
+		if (!cur_user) {
+			xbuf_cat(reply, "5:[ERROR]: no such user in store");
+			return HTTP_200_OK;
+		}
+		if (strcmp(cur_user->key, tmp_str)) {
+			xbuf_cat(reply, "6:[ERROR]: user key is not equal to stored one");
+			return HTTP_200_OK;
+		}
+		user_to_buf(reply, cur_user);
+		return HTTP_200_OK;
+
+	}
+   	else
+   	{
+		if (5 < method) {
+			xbuf_xcat(reply, "f:[ERROR]: such type(method=%d) of HTTP request is not served", method);
+			return HTTP_200_OK;
+		}
+		char *req_data = (char *) get_env(argv, REQ_ENTITY);
+		u64 content_len = (u64) get_env(argv, CONTENT_LENGTH);
+		u64 res = 0;
+		u64 fs_id = 0;
+		if ((1 >= content_len) || (MAX_REQ_DATA < content_len)) {
+			xbuf_xcat(reply, "7:[ERROR]: data size ={%d} is wrong, the range is [1..%d]", content_len, MAX_REQ_DATA);
+			return HTTP_200_OK;
+		}
+		if ((res = get_user_from_req(req_data, content_len, &req_user))) {
+			xbuf_xcat(reply, "8:[ERROR]: user data is not valid: parser error=[%d]", res);
+			return HTTP_200_OK;
+		}
+		cur_user = (struct F_item *) kv_get(&users, req_user.name, strlen(req_user.name));
+		switch (method) {
+			case 4: 	// PUT
+				if (!cur_user) {
+					xbuf_xcat(reply, "9:[ERROR]: no user to update");
+					return HTTP_200_OK;
+				}
 				strncpy(cur_user->key, req_user.key, KEY_SIZE-1);
 				cur_user->role = req_user.role;
-				cur_user->id = req_user.id;
+				//cur_user->id = req_user.id;
 				user_to_buf(reply, cur_user);
 				return HTTP_200_OK;
-			} else {
-				// TODO implement add user
+			case 3:		// PUST
+				if (cur_user) {
+					xbuf_xcat(reply, "a:[ERROR]: valid user provided in POST, use PUT to update");
+					return HTTP_200_OK;
+				}
 				// add new user
-				u64 fs_id = 0;
 				if (!req_user.id) {
+					// TODO create random and check for uniqueness
 					req_user.id = getms();
 				}
 				fs_id = add_instance(&fs_store, &req_user);
 				if (!fs_id) {
-					xbuf_cat(reply, "9:[ERROR]: add_instance fail");
+					xbuf_cat(reply, "b:[ERROR]: <f_store>: add_instance fail");
 					return HTTP_200_OK;
 				}
 				cur_user = get_item(&fs_store, fs_id);
 				if(0 != add_fsitem_to_kv(&users, cur_user)) {
-					xbuf_cat(reply, "a:[ERROR]: add_fsitem_to kv fail");
+					// TODO remove user from file storage
+					xbuf_cat(reply, "c:[ERROR]: add_fsitem_to kv fail");
 					return HTTP_200_OK;
 				}
 				user_to_buf(reply, cur_user);
 				return HTTP_200_OK;
-			}
+			case 5:
+				if (!cur_user) {
+					xbuf_cat(reply, "d:[ERROR]: remove user: no such user");
+					return HTTP_200_OK;
+				}
+				if (!(res = kv_del(&users, cur_user->name, strlen(cur_user->name)))) {
+					xbuf_cat(reply, "e:[ERROR]: remove from kv failed");
+				}
+				fs_id = (fs_store.data - cur_user)/(sizeof(struct F_item))+1;
+				delete_item(&fs_store, fs_id);
+				xbuf_cat(reply, "!");
+				return HTTP_200_OK;
+			default:
+				xbuf_xcat(reply, "f:[ERROR]: such type(method=%d) of HTTP request is not served", method);
+				return HTTP_200_OK;
 		}
-	   	else
-	   	{
-			// no other requests are served
-			xbuf_xcat(reply, "f:[ERROR]: such type(method=%d) of HTTP request is not served", method);
-			return HTTP_200_OK;
+	}
+}
+void write_all_users(struct F_store *store, xbuf_t *buffer)
+{
+	u64 counter = 1;
+	while (counter <= store->size) {
+		struct F_item *user = get_item(store, counter);
+		if (user) {
+			user_to_buf(buffer, user);
 		}
+		++counter;
+	}
 }
 void user_to_buf(xbuf_t *buf, struct F_item *user)
 {
-	printf("D1\n");
 	xbuf_xcat(buf, "|%lu:%s:%s:%d",
 						user->id,
 						user->name,
 						user->key,
 						user->role
 	);
-	printf("L1\n");
 }
 int get_user_from_req(char *data, u64 data_len, struct F_item *user)
 {
@@ -187,9 +214,10 @@ int get_user_from_req(char *data, u64 data_len, struct F_item *user)
 	u64 role = 0;
 	u64 offset = 0;
 	char cur_char = *(data+counter);
+	if (',' == cur_char) return 1;
 	while ((',' != cur_char) && (counter < data_len)) {
 		if ((NAME_SIZE-1) <= counter) {
-			return 1;
+			return 2;
 		}
 		*(user->name+counter) = cur_char;
 		++counter;
@@ -201,7 +229,7 @@ int get_user_from_req(char *data, u64 data_len, struct F_item *user)
 	while((',' != cur_char) && (counter < data_len)) {
 		key_counter = counter-offset;
 		if ((KEY_SIZE-1) <= key_counter) {
-			return 2;
+			return 3;
 		}
 		*(user->key+key_counter) = cur_char;
 		++counter;
@@ -209,11 +237,11 @@ int get_user_from_req(char *data, u64 data_len, struct F_item *user)
 	}
 	++counter;
 	if (counter >= data_len) {
-		return 3;
+		return 4;
 	}
 	if (!(role = strtoll(data+counter, NULL, 10))) {
 		// TODO extract more information from errno
-		return 4;
+		return 5;
 	}
 	user->role = role;
 	return 0;
@@ -303,16 +331,4 @@ int add_admin(kv_t *kv_store, struct F_store *fs_store)
 		return 3;
 	}
 	return 0;
-}
-int print_each(const kv_item *item, const void *reply)
-{
-	//struct F_item *user = (struct F_item *)item->val;
-	printf("D2: %p;\n", item->val);
-	printf("D22: %d;\n", item->klen);
-	printf("D3: %p;\n", item);
-	if (30 < item->klen) {
-		return 1;
-	}
-	user_to_buf((xbuf_t *)reply, (struct F_item *)item->val);
-	return 1;
 }
